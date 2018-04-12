64572abb86b1639529c0a54b8d8b893abdcaae63
abhishek-abhi <abhishekkumarmandal06@gmail.com>
2018-04-12 19:02:27 +0530

    Pro Project Start

---
+++ src/main/java/io/github/abhishekabhi/FixingDoctorsAppointmentSystemApplication.java
@@ -0,0 +1,12 @@
+package io.github.abhishekabhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixingDoctorsAppointmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FixingDoctorsAppointmentSystemApplication.class, args);
+	}
+}
