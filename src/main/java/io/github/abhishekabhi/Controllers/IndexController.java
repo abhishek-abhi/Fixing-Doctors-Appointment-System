542c8d82278322474b36ba2576e64516c7e75cf6
Abhishek Kumar Mandal <abhishekkumarmandal06@gmail.com>
2019-03-20 21:14:46 +0530

    Updated with comments

--- src/main/java/io/github/abhishekabhi/Controllers/IndexController.java
+++ src/main/java/io/github/abhishekabhi/Controllers/IndexController.java
@@ -3,9 +3,14 @@ package io.github.abhishekabhi.Controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 @Controller
 public class IndexController {
 
+	
+	/**
+	 * @return indexPage
+	 */
 	@RequestMapping("/")
 	public String indexPage() {
 		return "index";
