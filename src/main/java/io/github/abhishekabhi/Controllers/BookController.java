d0a2a87208e6ff3aef4d8e39519c4462ca2598d3
Abhishek Kumar Mandal <abhishekkumarmandal06@gmail.com>
2019-03-21 23:43:29 +0530

    new controller from new way

--- src/main/java/io/github/abhishekabhi/Controllers/BookController.java
+++ src/main/java/io/github/abhishekabhi/Controllers/BookController.java
@@ -2,6 +2,8 @@ package io.github.abhishekabhi.Controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+//This is BookController for project that is Spring
+
 @Controller
 public class BookController {
 
